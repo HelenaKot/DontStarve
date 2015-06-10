896ff5284eba7c14bff1b11651b98f1fcc23c4c9
Helena Kot <collab.cat@gmail.com>
2015-06-10 02:22:01 +0200

    nope

--- ds_mod/modinfo.lua
+++ ds_mod/modinfo.lua
@@ -5,22 +5,12 @@ version = "1.0" -- This is the version of the template. Change it to your own nu
 
 forumthread = ""
 
-
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 6
 
 dont_starve_compatible = true
-
 reign_of_giants_compatible = true
 
 -- TODO
 --icon_atlas = "modicon.xml"
-<<<<<<< HEAD
 --icon = "modicon.tex"
-=======
---icon = "modicon.tex"
---ToDo To
-
-
-
->>>>>>> origin/master
